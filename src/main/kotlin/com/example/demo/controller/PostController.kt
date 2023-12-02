d8544c68c512e144052cf3b2a031c408c7162d01
lyh990517 <lyh990517@naver.com>
2023-12-02 12:44:18 +0900

    crud restapi

--- src/main/kotlin/com/example/demo/controller/PostController.kt
+++ src/main/kotlin/com/example/demo/controller/PostController.kt
@@ -1,9 +1,10 @@
 package com.example.demo.controller
 
+import com.example.demo.entity.PostEntity
 import com.example.demo.service.PostService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -13,21 +14,49 @@ class PostController {
     @Autowired
     private lateinit var postService: PostService
 
-    @GetMapping("")
-    fun readPosts() = postService.getAllPosts()
-
-    @PostMapping("/create")
-    fun createPost(
-            @RequestParam name: String,
-            @RequestParam age: Int
-    ): String {
-        postService.createPost(name, age)
-        return ""
+    @GetMapping
+    fun getAllPosts(): ResponseEntity<List<PostEntity>> {
+        val posts = postService.getAllPosts()
+        return ResponseEntity(posts, HttpStatus.OK)
     }
 
-    @GetMapping("/edit")
-    fun editPage() = "post/edit"
+    @PostMapping
+    fun createPost(@RequestParam name: String, @RequestParam age: Int): ResponseEntity<PostEntity> {
+        val createdPost = postService.createPost(name, age)
+        return ResponseEntity(createdPost, HttpStatus.CREATED)
+    }
 
-    @GetMapping("/delete")
-    fun deletePage() = "post/delete"
+    @GetMapping("/{id}")
+    fun getPostById(@PathVariable id: Long): ResponseEntity<PostEntity?> {
+        val post = postService.getPostById(id)
+        return if (post != null) {
+            ResponseEntity(post, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @PutMapping("/{id}")
+    fun updatePost(
+            @PathVariable id: Long,
+            @RequestParam name: String,
+            @RequestParam age: Int
+    ): ResponseEntity<PostEntity?> {
+        val updatedPost = postService.updatePost(id, name, age)
+        return if (updatedPost != null) {
+            ResponseEntity(updatedPost, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletePost(@PathVariable id: Long): ResponseEntity<Unit> {
+        val isDeleted = postService.deletePost(id)
+        return if (isDeleted) {
+            ResponseEntity(HttpStatus.NO_CONTENT)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
 }
